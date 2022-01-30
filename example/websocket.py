0fd971508731c69d8089a175ba416abecbd4eb1c
Bas van den Aakster <bvdaakster@gmail.com>
2022-01-30 13:35:52 +0100

    Improve websocket example

--- example/websocket.py
+++ example/websocket.py
@@ -5,13 +5,20 @@ from ftx import FtxClient
 
 async def main():
     ftx = FtxClient()
+    # connect the websocket
     await ftx.websocket.connect()
 
     first_market = ftx.get_markets()[0]
+    # subscribe to the trades channel of first available market
     await ftx.websocket.subscribe('trades', first_market['name'])
 
     while ftx.websocket.connected:
-        print(await ftx.websocket.recv())
+        # block and wait for next message
+        msg = await ftx.websocket.recv()
+        if 'channel' in msg and msg['channel'] == 'trades':
+            if 'data' in msg:
+                # print trade
+                print(msg['market'], msg['data'][0]['side'], msg['data'][0]['size'])
 
 
 if __name__ == '__main__':
